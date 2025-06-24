fc5315bf347dd873199c48c250bc906d09f2b3dd
Haoran Xie <haoranxie2005@gmail.com>
2025-06-24 08:02:10 -0400

    .clang-format configurations

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
-#include <iostream>
-#include <cstdint>
 #include <cmath>
+#include <cstdint>
 #include <gsl/gsl>
+#include <iostream>
 
-int main(std::int32_t argc, gsl::zstring* argv)
-{
-	gsl::span<gsl::zstring> arguments(argv, argc);
-	std::sort(arguments.begin(), arguments.end());
+struct Point {
+  std::float_t x, y, z;
 
+  void MyMethod() const {}
+};
 
-	std::uint32_t count = 0;
-	std::size_t size = 12;
+int main(std::int32_t argc, gsl::zstring* argv) {
+  gsl::span<gsl::zstring> arguments(argv, argc);
+  std::sort(arguments.begin(), arguments.end());
 
-	std:float_t s = 0.0f;
+  std::uint32_t count = 0;
+  std::size_t size = 12;
 
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+std:
+  float_t s = 0.0f;
+
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
 }
